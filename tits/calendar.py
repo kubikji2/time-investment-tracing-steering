c68f97517b84cf84491ac6dc91455f064b77ef77
Jiří Kvant Kubík <jiri.kub@gmail.com>
2023-01-06 18:49:50 +0100

    [tits] adding calendar.

---
+++ tits/calendar.py
@@ -0,0 +1,22 @@
+
+from .icream.icream import calendar 
+from . import utils
+from . import event
+
+class TITSCalendar(calendar.ICREAMCalendar):
+
+    def __init__(self, path=None, calendar=None):
+        super().__init__(path, calendar)
+        self.process_events(self.events)
+    
+    def process_events(self, events):
+        filtered = []
+        for ev in events:
+            if utils.is_parsable_event(ev):
+                _tag, _category = utils.parse_event(ev)
+                _ev = event.TITSEvent(tag=_tag,category=_category, ev=ev)
+                filtered.append(_ev)
+            pass
+            
+        self.events = filtered
+
