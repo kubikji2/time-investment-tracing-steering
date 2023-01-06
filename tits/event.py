cc99b0f3d96f73a829c54ecdbc932bb04af9d50f
Jiří Kvant Kubík <jiri.kub@gmail.com>
2023-01-06 18:47:24 +0100

    [tits] adding event.

---
+++ tits/event.py
@@ -0,0 +1,16 @@
+
+from .icream.icream import event
+
+class TITSEvent(event.ICREAMEvent):
+
+    def __init__(self, tag, category="", ev : event.ICREAMEvent = None, start=None, end=None, name="", organizer="", description="", component=None):
+        if ev is not None:
+            start = ev.start
+            end = ev.end
+            name = ev.name
+            organizer = ev.organizer
+            description = ev.description
+            component = ev.component
+        super().__init__(start, end, name, organizer, description, component)
+        self.tag = tag
+        self.category = category
