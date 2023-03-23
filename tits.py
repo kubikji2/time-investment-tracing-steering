f5d91a6a5b927ee3612526c44759fc440c2d1eeb
Jiří Kvant Kubík <jiri.kub@gmail.com>
2023-03-23 16:38:58 +0100

    [tits] testing filtering and adding categories.

--- tits.py
+++ tits.py
@@ -4,7 +4,10 @@ from tits import calendar as tc
 from tits import categories
 from tits import time_investment as ti
 
+import datetime
+
 if __name__=="__main__":
+    # dummy path
     _path = "./kubikji2@fel.cvut.cz.ics"
     tits_cal = tc.TITSCalendar(path=_path)
     tits_cal.sort_calendar_by_date()
@@ -12,9 +15,13 @@ if __name__=="__main__":
     tits_cat = categories.TITSCategories("./categories.tcf")
 
     tits_ti = ti.TITSTimeInvestment([tits_cal], tits_cat)
-    tits_ti.print_time_investment()
-    #for ev in icrm_cal.events:
-        #tag, activity = utils.parse_event(ev)
-        #print(tag, activity)
+
+    _start = datetime.datetime(2023,3,20,0,0,0, tzinfo=datetime.timezone(datetime.timedelta(0,0,0,0,0,1)))
+    _end = datetime.datetime(2023,3,27,0,0,0, tzinfo=datetime.timezone(datetime.timedelta(0,0,0,0,0,1)))
+    tits_ti.print_time_investment(_start, _end, True)
+    
+    #for ev in tits_ti.events:
+    #    tag, activity = utils.parse_event(ev)
+    #    print(tag, activity)
 
     
