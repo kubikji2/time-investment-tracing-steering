8c370c3ad39ee07747eb281401538482f5b9562d
Jiří Kvant Kubík <jiri.kub@gmail.com>
2023-03-23 16:19:42 +0100

    [categories] experimenting and adding comments.

--- tits/categories.py
+++ tits/categories.py
@@ -1,6 +1,8 @@
 
 from . import utils
 
+# load dictionary from the file using the utils
+# then process the file
 def load_categories(path):
     loaded_dic = utils.read_dictionary_file(path=path)
     inv_dic = {}
@@ -9,6 +11,20 @@ def load_categories(path):
         tags = [val.strip().upper() for val in data.split(",")]
         for tag in tags:
             inv_dic[tag] = key
+            #if len(tag) > 0 and tag[0] == "$":
+            #    print(tag)
+    
+    """
+    for key in inv_dic.keys():
+        if key[0] == "$":
+            print("{} : {}".format(key, inv_dic[key]))
+            new_key = key[1:]
+            print(new_key)
+            for _key in inv_dic.keys():
+                if inv_dic[_key].upper() == new_key:
+                    print("to update {} : {} -> {}".format(_key, inv_dic[_key], inv_dic[key]))
+    """
+
     return inv_dic
 
 
